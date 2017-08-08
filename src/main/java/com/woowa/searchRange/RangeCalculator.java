d6fe7ab37dbf60c51de0d80c544e17056b22505d
buk4130 <buk4130@gmail.com>
2017-08-08 18:46:12 +0900

    [이슈 #1] 위경도 범위 추가 (#2)

--- src/main/java/com/woowa/searchRange/RangeCalculator.java
+++ src/main/java/com/woowa/searchRange/RangeCalculator.java
@@ -8,8 +8,21 @@ import com.woowa.model.SearchRange;
 public class RangeCalculator {
 
     SearchRange searchRange;
+    double latitudeRange;
+    double longitudeRange;
+
+    public RangeCalculator(double latitudeRange, double longitudeRange) {
+        this.latitudeRange = latitudeRange;
+        this.longitudeRange = longitudeRange;
+    }
 
     public SearchRange getSearchRange(Float[] latlon) {
+        String rightLatitude = String.valueOf(latlon[0] + latitudeRange);
+        String leftLatitude = String.valueOf(latlon[0] - latitudeRange);
+        String topLongitude = String.valueOf(latlon[1] + longitudeRange);
+        String lowLongitude = String.valueOf(latlon[1] - longitudeRange);
+
+        searchRange = new SearchRange(rightLatitude, leftLatitude, topLongitude, lowLongitude);
 
         return searchRange;
     }
